da314f74eb480f313ef0e6052dc6f3f98f34623d
Tuomas Rautanen <e1600750@edu.vamk.fi>
2019-04-02 13:59:54 +0300

    added documentation to CustomException and ExType

--- CustomException.java
+++ CustomException.java
@@ -1,5 +1,11 @@
 package minesweeper;
 
+/**
+ * Makes custom exceptions possible for program
+ * 
+ * @author Tuomas Rautanen
+ *
+ */
 public class CustomException extends Exception {
 	
 	/**
@@ -7,8 +13,14 @@ public class CustomException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private ExType type;
+	private ExType type; // type of exception
 	
+	/**
+	 * Class for custom made exceptions used by program
+	 * 
+	 * @param msg exception message
+	 * @param e type of exception
+	 */
 	public CustomException(String msg, ExType e) {
 		
         super(msg);
--- ExType.java
+++ ExType.java
@@ -1,6 +1,14 @@
 package minesweeper;
-
+/**
+ * Has custom exception types used by program
+ * 
+ * <li>{@link #TILESTATE}</li>
+ * 
+ * @author Tuomas Rautanen
+ */
 public enum ExType {
-	
-	TILESTATE, OUTOFRANGE
+	/**
+	 * Tile state isn't initialised correctly
+	 */
+	TILESTATE
 }
